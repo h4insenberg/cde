7a379e1aa84f67eb37ecfc0661357fccbe4b02d9
h4insenberg <85884280+h4insenberg@users.noreply.github.com>
2025-08-19 09:19:08 -0300

    Adicionar useEffect à importação do React

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BusinessProvider } from './context/BusinessContext';
 import { Header } from './components/Layout/Header';
 import { BottomNavigation } from './components/Layout/BottomNavigation';
