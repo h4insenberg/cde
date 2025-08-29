14ed42e5821f20e89c647b9099586c53fbfd9d9a
h4insenberg <85884280+h4insenberg@users.noreply.github.com>
2025-08-29 02:57:49 -0300

    Updated LoanForm.tsx

--- src/components/Loans/LoanForm.tsx
+++ src/components/Loans/LoanForm.tsx
@@ -495,6 +495,7 @@ export function LoanForm({ loan, onSave, onCancel }: LoanFormProps) {
               </div>
             )}
           </div>
+        </div>
         
         {/* Actions */}
         <div className="flex flex-col sm:flex-row gap-3 p-4 sm:p-6 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-[#18191c] flex-shrink-0">
