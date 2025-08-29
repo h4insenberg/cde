095bca02b6acb435a69fb57eaf2f4d85fb83116d
h4insenberg <85884280+h4insenberg@users.noreply.github.com>
2025-08-29 02:57:00 -0300

    Updated ExitForm.tsx

--- src/components/Exits/ExitForm.tsx
+++ src/components/Exits/ExitForm.tsx
@@ -268,6 +268,7 @@ export function ExitForm({ exit, onSave, onCancel }: ExitFormProps) {
               </p>
             </div>
           </div>
+        </div>
         
         {/* Actions */}
         <div className="flex flex-col sm:flex-row gap-3 p-4 sm:p-6 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-[#18191c] flex-shrink-0">
